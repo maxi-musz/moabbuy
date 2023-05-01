7839366fa3b70a6c52c2efcd49e584127b43c5c7
Moab <adeyeye.toyorsi@gmail.com>
2023-05-01 08:23:48 +0100

    first commit

---
+++ utils/tokens.js
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+export const createActivationToken = (payload) => {
+  return jwt.sign(payload, process.env.ACTIVATION_TOKEN_SECRET, {
+    expiresIn: "6h",
+  });
+};
+export const createResetToken = (payload) => {
+  return jwt.sign(payload, process.env.RESET_TOKEN_SECRET, {
+    expiresIn: "6h",
+  });
+};
+  
